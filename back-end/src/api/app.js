a95b59a4c9569cc8d6c789c6c56b85ca710915c1
João Hélder <joaoheldermartins@hotmail.com>
2023-03-29 17:20:25 -0300

    feat: cria a validação do login no banco de dados

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const loginRouter = require('./routes/loginRouter');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(express.json());
+app.use('/login', loginRouter);
 
 module.exports = app; 
---
+++ back-end/src/api/services/loginService.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const md5 = require('md5');
+const auth = require('../auth/authService');
+const usersService = require('./usersService');
+
+const newLogin = async (email, password) => {  
+  const passwordHash = md5(password);
+  console.log(passwordHash);
+  const user = await usersService.getUserByEmail(email);
+  
+  if (!user || user.password !== passwordHash) {
+    return { type: 'INVALID_FIELDS', message: 'Invalid fields' }; 
+  }
+
+  const token = jwt.sign({ data: { userId: user.id } }, auth.secret, { expiresIn: auth.expires });
+
+  const result = {
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token,
+  };
+
+  return result;
+};
+
+module.exports = {
+  newLogin,
+};
