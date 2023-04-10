393bd57c456d04317d965da0e917d0f5a52f45bf
João Hélder <joaoheldermartins@hotmail.com>
2023-04-10 18:15:55 -0300

    refactor: retira parte comentada da 'navbar'

--- front-end/src/Components/NavBar.jsx
+++ front-end/src/Components/NavBar.jsx
@@ -57,32 +57,4 @@ NavBar.propTypes = {
   }),
 }.isRequired;
 
-// export default Navbar;
-// import React, { useContext } from 'react';
-// import { Link } from 'react-router-dom';
-// import MyContext from '../context/MyContext';
-
-// function NavBar() {
-//   const { logOut } = useContext(MyContext);
-//   const user = localStorage.getItem('user');
-
-//   return (
-//     <div>
-//       <li data-testid="customer_products__element-navbar-link-orders">
-//         <Link to="/seller/orders">GERENCIAR USUÁRIOS</Link>
-//       </li>
-//       <li data-testid="customer_products__element-navbar-user-full-name">
-//         {user.name}
-//       </li>
-//       <button
-//         type="button"
-//         onClick={ logOut }
-//         data-testid="customer_products__element-navbar-link-logout"
-//       >
-//         Sair
-//       </button>
-//     </div>
-//   );
-// }
-
 export default NavBar;
