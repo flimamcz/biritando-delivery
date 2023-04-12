492be485fed3a83facc9b41336f7f15a0e852fe0
João Hélder <joaoheldermartins@hotmail.com>
2023-04-12 17:18:22 -0300

    feat: adiciona o css da pagina de admin

--- front-end/src/Pages/Admin.jsx
+++ front-end/src/Pages/Admin.jsx
@@ -3,10 +3,11 @@ import AdminForm from '../Components/AdminForm';
 import { navBarAdmin } from '../utils/navBarinfo';
 import Navbar from '../Components/NavBar';
 import UsersTable from '../Components/UsersTable';
+import '../styles/adminHeader.css';
 
 function Admin() {
   return (
-    <div>
+    <div className="admin-container">
       <Navbar type={ navBarAdmin } />
       <AdminForm />
       <UsersTable />
